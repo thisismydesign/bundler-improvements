a5bd5852db807af863dae892609ea047bd8564d2
Csaba Apagyi <csaba.apagyi@gmail.com>
2017-11-10 15:24:50 +0100

    Doesn't run coverage when ran by guard

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,15 @@
 # The following lines should be added after `require "bundler/setup"`
 
-require 'simplecov'
-SimpleCov.add_filter ['spec', 'config']
-require "coveralls"
-Coveralls.wear!
+def ran_by_guard
+  ARGV.any? {|e| e =~ /guard-rspec/ }
+end
+
+unless ran_by_guard
+  require 'simplecov'
+  SimpleCov.add_filter ['spec', 'config']
+  require "coveralls"
+  Coveralls.wear!
+end
 
 # The following lines should be added at the end`
 
